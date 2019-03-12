3954d6789c407d450479e5a8eb7b7a33e7ed6834
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-03-12 08:57:55 +0100

    Adding main files and project initialization

---
+++ app.js
@@ -0,0 +1,21 @@
+// Import depedencies
+const express = require('express');
+const http = require('http');
+// const url = require('url');
+const { argv } = require('yargs');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongoose');
+
+// Import models
+const { mongoose } = require('./db/db');
+
+const app = express();
+app.use(bodyParser.json());
+
+const inputPort = argv._[0];
+const port = (inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 == 0)) ? inputPort : 3000;
+
+http.Server(app);
+
+console.log(`Server is running on port : ${port} \n You can access to the server at the following address : http://localhost:${port}`);
+app.listen(port);
---
+++ db/db.js
@@ -0,0 +1,11 @@
+// Connexion 
+const mongoose = require('mongoose');
+const {privateurl} = require('./privateurl');
+
+const url = privateurl;
+mongoose.Promise = global.Promise;
+mongoose.connect(url, { useNewUrlParser: true });
+
+
+// Exportation
+module.exports = { mongoose }
