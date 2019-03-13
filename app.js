3fdb43fd5372bb086d94d694ffb011fe4e4e762e
Kwoak <maisonneuve.henry@gmail.com>
2019-03-13 17:36:15 +0100

    fixed get problem

--- app.js
+++ app.js
@@ -5,68 +5,39 @@ const access = require("./access/access.js");
 
 // Port argument, if nothing entered, default port is 3000
 const inputPort = argv.port;
-const inputFct = argv.fct;
-const inputTable = argv.table;
-let port = (inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0 && inputPort < 65536)) ? inputPort : 3000;
+const inputGet = argv.get;
+let port =
+  inputPort &&
+  !isNaN(inputPort) &&
+  (inputPort > 0 && inputPort % 1 === 0 && inputPort < 65536)
+    ? inputPort
+    : 3000;
 
 // Function and table argument, the user must enter one of the functions (get, post, delete)
 // and one of the tables (characters, houses, movies)
-switch (inputFct) {
-  case "get":
-    switch (inputTable) {
-      case "characters":
-        access.getCharacters;
-        break;
-      case "houses":
-        access.getHouses;
-        break;
-      case "movies":
-        access.getMovies;
-        break;
-      default:
-        console.log("You must enter 'characters', 'houses' or 'movies'.");
-    }
+switch (inputGet) {
+  case "characters":
+    access.getCharacters;
     break;
-  case "post":
-    switch (inputTable) {
-      case "characters":
-        access.postCharacters;
-        break;
-      case "houses":
-        access.postHouses;
-        break;
-      case "movies":
-        access.postMovies;
-        break;
-      default:
-        console.log("You must enter 'characters', 'houses' or 'movies'.");
-    }
+  case "houses":
+    access.getHouses;
     break;
-  case "delete":
-    switch (inputTable) {
-      case "characters":
-        access.deleteCharacter;
-        break;
-      case "houses":
-        access.deleteHouse;
-        break;
-      case "movies":
-        access.deleteMovie;
-        break;
-      default:
-        console.log("You must enter 'characters', 'houses' or 'movies'.");
-    }
+  case "movies":
+    access.getMovies;
     break;
-  default:
-    console.log("You must enter 'get', 'post' or 'delete'.");
 }
 
 //Launch server on port given by the user
 
 http.Server(access.app);
-
 access.app.listen(port, () => {
-    console.log(`Server is running on port : ${port} \n You can access to the server at the following address : http://localhost:${port}/${inputTable}`);
+  if (inputGet) {
+    console.log(
+      `Server is running on port : ${port} \n You can access to the server at the following address : http://localhost:${port}/${inputGet}`
+    );
+  } else {
+    console.log(`Server is running on port : ${port} \n You can access to the server at the following address : http://localhost:${port}`);
+  }
 });
 
 module.exports = {};
